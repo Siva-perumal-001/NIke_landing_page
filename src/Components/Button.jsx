8cffc320ce4c5a2c8eed95c430169be9647d8eba
Siva-perumal-001 <sivaperumal.s.spl@gmail.com>
2025-11-13 11:44:19 +0530

    Subscribe section was fully done

--- src/Components/Button.jsx
+++ src/Components/Button.jsx
@@ -1,10 +1,10 @@
-const Button = ({label,iconURL,backgroundColor,borderColor,textColor}) => {
+const Button = ({label,iconURL,backgroundColor,borderColor,textColor,fullWidth}) => {
   return (
     <button 
     className={`text-lg border leading-none font-montserrat flex justify-center 
         items-center px-7 py-4 gap-2
         ${backgroundColor ? `${backgroundColor} ${textColor} ${borderColor}`
-        : `bg-coral-red border-coral-red text-white`} rounded-full`}
+        : `bg-coral-red border-coral-red text-white`} rounded-full ${fullWidth && 'w-full'}"}`}
     >
         {label}
         {iconURL && <img src={iconURL} alt="icon" className="ml-2 rounded-full w-5 h-5" />}
