6c0595706c94fc4dfeeeccd99f53e8ab586667f9
Siva-perumal-001 <sivaperumal.s.spl@gmail.com>
2025-11-13 09:43:24 +0530

    Services section was done

--- src/Sections/Services.jsx
+++ src/Sections/Services.jsx
@@ -1,6 +1,13 @@
+import { services } from './../Constants/index';
+import ServiceCard from '../Components/ServiceCard';
+
 const Services = () => {
   return (
-    <div>Services</div>
+    <section className=' max-conatainer justify-center flex flex-wrap gap-9'>
+      {services.map((service)=>(
+        <ServiceCard key={service.label} {...service}/>
+      ))}
+    </section>
   )
 }
 
